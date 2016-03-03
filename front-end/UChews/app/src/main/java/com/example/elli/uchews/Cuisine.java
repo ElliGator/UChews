78957d2e8124f6b9e4ab2df7db6aa19b4a153e99
ElliGator <ecarranza@ufl.edu>
2016-03-03 10:59:18 -0500

    Fixed conflicts in Cuisine

--- front-end/UChews/app/src/main/java/com/example/elli/uchews/Cuisine.java
+++ front-end/UChews/app/src/main/java/com/example/elli/uchews/Cuisine.java
@@ -4,41 +4,23 @@ package com.example.elli.uchews;
  * Created by Chris on 2/18/2016.
  */
 public enum Cuisine {
-<<<<<<< HEAD
-    AMERICAN ("348", "American", R.mipmap.ic_bbq, false),
-    BBQ ("349", "BBQ", R.mipmap.ic_bbq, false),
-    BURGERS ("351", "Burgers", R.mipmap.ic_bbq, false),
-    CHINESE ("352", "Chinese", R.mipmap.ic_chinese, false),
-    FRENCH ("356", "French", R.mipmap.ic_chinese, false),
-    INDIAN ("357", "Indian", R.mipmap.ic_chinese, false),
-    ITALIAN ("358", "Italian", R.mipmap.ic_seafood, false),
-    JAPANESE ("359", "Japanese", R.mipmap.ic_pizza, false),
-    KOREAN ("360", "Korean", R.mipmap.ic_pizza, false),
-    MEXICAN ("361","Mexican", R.mipmap.ic_chinese, false),
-    MIDDLE_EASTERN ("362", "Middle Eastern", R.mipmap.ic_pizza, false),
-    PIZZA ("363", "Pizza", R.mipmap.ic_pizza, false),
-    SEAFOOD ("364", "Seafood", R.mipmap.ic_seafood, false),
-    SUSHI ("366", "Sushi", R.mipmap.ic_seafood, false),
-    THAI ("367", "Thai", R.mipmap.ic_chinese, false),
-    VEGETARIAN ("368","Vegetarian", R.mipmap.ic_bbq, false),
-=======
-    AMERICAN (348, "American", R.mipmap.ic_bbq),
-    BBQ (349, "BBQ", R.mipmap.ic_bbq),
-    BURGERS (351, "Burgers", R.mipmap.ic_bbq),
-    CHINESE (352, "Chinese", R.mipmap.ic_chinese),
-    FRENCH (356, "French", R.mipmap.ic_chinese),
-    INDIAN (357, "Indian", R.mipmap.ic_chinese),
-    ITALIAN (358, "Italian", R.mipmap.ic_seafood),
-    JAPANESE (359, "Japanese", R.mipmap.ic_pizza),
-    KOREAN (360, "Korean", R.mipmap.ic_pizza),
-    MEXICAN (361,"Mexican", R.mipmap.ic_chinese),
-    MIDDLE_EASTERN (362, "Middle Eastern", R.mipmap.ic_pizza),
-    PIZZA (363, "Pizza", R.mipmap.ic_pizza),
-    SEAFOOD (364, "Seafood", R.mipmap.ic_seafood),
-    SUSHI (366, "Sushi", R.mipmap.ic_seafood),
-    THAI (367, "Thai", R.mipmap.ic_chinese),
-    VEGETARIAN (368,"Vegetarian", R.mipmap.ic_bbq),
->>>>>>> origin/elli-front-end
+    AMERICAN (348, "American", R.mipmap.ic_bbq, false),
+    BBQ (349, "BBQ", R.mipmap.ic_bbq, false),
+    BURGERS (351, "Burgers", R.mipmap.ic_bbq, false),
+    CHINESE (352, "Chinese", R.mipmap.ic_chinese, false),
+    FRENCH (356, "French", R.mipmap.ic_chinese, false),
+    INDIAN (357, "Indian", R.mipmap.ic_chinese, false),
+    ITALIAN (358, "Italian", R.mipmap.ic_seafood, false),
+    JAPANESE (359, "Japanese", R.mipmap.ic_pizza, false),
+    KOREAN (360, "Korean", R.mipmap.ic_pizza, false),
+    MEXICAN (361,"Mexican", R.mipmap.ic_chinese, false),
+    MIDDLE_EASTERN (362, "Middle Eastern", R.mipmap.ic_pizza, false),
+    PIZZA (363, "Pizza", R.mipmap.ic_pizza, false),
+    SEAFOOD (364, "Seafood", R.mipmap.ic_seafood, false),
+    SUSHI (366, "Sushi", R.mipmap.ic_seafood, false),
+    THAI (367, "Thai", R.mipmap.ic_chinese, false),
+    VEGETARIAN (368,"Vegetarian", R.mipmap.ic_bbq, false),
+
 
     // It may be better to make these ones "Dining Styles" as opposed to cuisines //
     /*BUFFETS (350, "Buffets"),
@@ -64,13 +46,10 @@ public enum Cuisine {
      */
     private final int image;
 
-<<<<<<< HEAD
     private boolean inWheel;
 
-    Cuisine(String factual_id, String name, int image, boolean inWheel) {
-=======
-    Cuisine(int factual_id, String name, int image) {
->>>>>>> origin/elli-front-end
+
+    Cuisine(int factual_id, String name, int image, boolean inWheel) {
         this.name = name;
         this.factual_id = factual_id;
         this.image = image;
@@ -90,7 +69,6 @@ public enum Cuisine {
         return image;
     }
 
-<<<<<<< HEAD
     public boolean isInWheel(){
         return inWheel;
     }
@@ -99,10 +77,7 @@ public enum Cuisine {
         this.inWheel = val;
     }
 
-    public static Cuisine getCuisineById(String cuisine_id){
-=======
     public static Cuisine getCuisineById(int cuisine_id){
->>>>>>> origin/elli-front-end
         for(Cuisine c: Cuisine.values()){
             if(c.factual_id == cuisine_id)
                 return c;
