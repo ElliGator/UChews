6d84bec568f4024a1717cfcef0d9526264253ba2
cwhitten1 <cwhitten@ufl.edu>
2016-03-06 14:19:58 -0500

    Fix get user history function to return json result

--- back-end/functions/history/get_user_history.sql
+++ back-end/functions/history/get_user_history.sql
@@ -1,4 +1,8 @@
 CREATE OR REPLACE FUNCTION 
-	get_user_history(email char) RETURNS SETOF history AS $$
-		SELECT * FROM history WHERE history.user_email = email;
-	$$ LANGUAGE SQL;
+	get_user_history(email varchar)
+	RETURNS json AS $$
+	BEGIN
+		CREATE TEMP TABLE user_hist ON COMMIT DROP AS SELECT * FROM history WHERE history.user_email = $1;
+		RETURN (SELECT array_to_json(array_agg(user_hist)) FROM user_hist);
+	END;
+	$$ LANGUAGE plpgsql;  
